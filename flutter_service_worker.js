255f652a85eb07b54cb67f003c36d5b2030bb25a
shoroardiSumon <sumonpust7@gmail.com>
2025-11-15 13:52:03 +0600

    Add version in footerb

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "de87186b469c6cd5fefb477013d8b98f
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "ab15f96224dd343ee4ea9573a6d00cf1",
+"flutter_bootstrap.js": "68f0a6d8eb8328763e347ecb2d05586d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
