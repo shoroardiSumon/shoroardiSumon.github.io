4b421d64d335a0281b1ab93c886ef1e2d75307e8
shoroardiSumon <sumonpust7@gmail.com>
2025-11-28 12:33:25 +0600

    added version in footer

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "de87186b469c6cd5fefb477013d8b98f
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "89dbadf541e046e20b18cef1471b9f7a",
+"flutter_bootstrap.js": "662a08aba95fa44f1056cf99e28197bb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
